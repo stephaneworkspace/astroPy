ff31e0d07dff2c953d0eee8371041ef79b2a2696
Stéphane Bressani <s.bressani@bluewin.ch>
2019-10-11 20:38:36 +0200

    backup

--- s_zodiac.py
+++ s_zodiac.py
@@ -5,6 +5,7 @@ class Zodiac:
         self.id = id
         self.nom = nom
         self.symbol = self.switchSymbol(nom)
+        self.element = self.switchElement(nom)
         self.svg = 'assets/svg/zodiac/' + nom + '.svg'
 
     def switchSymbol(self, nom):
@@ -21,5 +22,22 @@ class Zodiac:
             const.CAPRICORN: '♑',
             const.AQUARIUS: '♒',
             const.PISCES: '♑'
+        }
+        return switcher.get(nom, '?')
+    
+    def switchElement(self, nom):
+        switcher = {
+            const.ARIES: 'Feu',
+            const.TAURUS: 'Terre',
+            const.GEMINI: 'Air',
+            const.CANCER: 'Eau',
+            const.LEO: 'Feu',
+            const.VIRGO: 'Terre',
+            const.LIBRA: 'Air',
+            const.SCORPIO: 'Eau',
+            const.SAGITTARIUS: 'Feu',
+            const.CAPRICORN: 'Terre',
+            const.AQUARIUS: 'Air',
+            const.PISCES: 'Eau'
         }
         return switcher.get(nom, '?')
