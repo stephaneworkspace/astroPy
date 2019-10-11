f50828bbfd49815c7a060af44124ef328af0ebd0
Stéphane Bressani <s.bressani@bluewin.ch>
2019-10-11 21:26:35 +0200

    backup

--- s_zodiac.py
+++ s_zodiac.py
@@ -17,42 +17,79 @@ class Zodiac:
         switcher = {
             const.ARIES: id,
             const.TAURUS: self.switchAscTaurus(id),
+            const.GEMINI: self.switchAscGemini(id),
+            const.CANCER: self.switchAscCancer(id),
             const.AQUARIUS: self.switchAscAquarius(id)
         }
         return switcher.get(ascSign, '?')
     
     def switchAscTaurus(self, id):
         switcher = {
-            2: 1,
-            3: 2,
-            4: 3,
-            5: 4,
-            6: 5,
-            7: 6,
-            8: 7,
-            9: 8,
-            10: 9,
-            11: 10,
-            12: 11,
-            1: 11 + 1,
+            const.ID_TAURUS: 1,
+            const.ID_GEMINI: 2,
+            const.ID_CANCER: 3,
+            const.ID_LEO: 4,
+            const.ID_VIRGO: 5,
+            const.ID_LIBRA: 6,
+            const.ID_SCORPIO: 7,
+            const.ID_SAGITTARIUS: 8,
+            const.ID_CAPRICORN: 9,
+            const.ID_AQUARIUS: 10,
+            const.ID_PISCES: 11,
+            const.ID_ARIES: 11 + 1,
 
+        }
+        return switcher.get(id, '?')
+
+    def switchAscGemini(self, id):
+        switcher = {
+            const.ID_GEMINI: 1,
+            const.ID_CANCER: 2,
+            const.ID_LEO: 3,
+            const.ID_VIRGO: 4,
+            const.ID_LIBRA: 5,
+            const.ID_SCORPIO: 6,
+            const.ID_SAGITTARIUS: 7,
+            const.ID_CAPRICORN: 8,
+            const.ID_AQUARIUS: 9,
+            const.ID_PISCES: 10,
+            const.ID_ARIES: 10 + 1,
+            const.ID_TAURUS: 10 + 2
+
+        }
+        return switcher.get(id, '?')
+
+    def switchAscCancer(self, id):
+        switcher = {
+            const.ID_CANCER: 1,
+            const.ID_LEO: 2,
+            const.ID_VIRGO: 3,
+            const.ID_LIBRA: 4,
+            const.ID_SCORPIO: 5,
+            const.ID_SAGITTARIUS: 6,
+            const.ID_CAPRICORN: 7,
+            const.ID_AQUARIUS: 8,
+            const.ID_PISCES: 9,
+            const.ID_ARIES: 9 + 1,
+            const.ID_TAURUS: 9 + 2,
+            const.ID_GEMINI: 9 + 3
         }
         return switcher.get(id, '?')
 
     def switchAscAquarius(self, id):
         switcher = {
-            11: 1,
-            12: 2,
-            1: 2 + 1,
-            2: 2 + 1,
-            3: 2 + 3,
-            4: 2 + 4,
-            5: 2 + 5,
-            6: 2 + 6,
-            7: 2 + 7,
-            8: 2 + 8,
-            9: 2 + 9,
-            10: 2 + 10,
+            const.ID_AQUARIUS: 1,
+            const.ID_PISCES: 2,
+            const.ID_ARIES: 2 + 1,
+            const.ID_TAURUS: 2 + 1,
+            const.ID_GEMINI: 2 + 3,
+            const.ID_CANCER: 2 + 4,
+            const.ID_LEO: 2 + 5,
+            const.ID_VIRGO: 2 + 6,
+            const.ID_LIBRA: 2 + 7,
+            const.ID_SCORPIO: 2 + 8,
+            const.ID_SAGITTARIUS: 2 + 9,
+            const.ID_CAPRICORN: 2 + 10,
 
         }
         return switcher.get(id, '?')
