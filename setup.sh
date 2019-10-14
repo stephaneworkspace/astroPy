39a1a747b15c8f2cb7c114c36c240687fca74788
stephanebressani <s.bressani@bluewin.ch>
2019-10-15 00:33:01 +0200

    clean interface between python python and dart

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ cd ..
 python3 setup.py install
 rm -rf astro_py_texte
 # Repository private for text on french
-git clone https://github.com/stephaneworkspace/astro_py_texte.git
+git clone https://github.com/stephaneworkspace/astro_py_text.git
+cd astro_py_text
+python3 setup.py install
