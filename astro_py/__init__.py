632d68ae73a4d59b8ee6d67e13feefbe6cc13cbd
stephanebressani <s.bressani@bluewin.ch>
2019-10-16 04:00:03 +0200

    change version

--- astro_py/__init__.py
+++ astro_py/__init__.py
@@ -6,7 +6,7 @@
 import os
 
 
-__version__ = '0.0.12.dev7'
+__version__ = '0.0.12.dev8'
 
 # Library and resource paths
 PATH_LIB = os.path.dirname(__file__) + os.sep
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages
 setup(
     # Project
     name = 'astro_py',
-    version = '0.0.12.dev7',
+    version = '0.0.12.dev8',
     
     # Sources
     # packages = ['astro_py'],
